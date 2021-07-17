16a6eabaf11585ebede619facef22c131f404e7b
Amit Goldenberg <amit.wifi@gmail.com>
2021-07-17 17:27:43 +0300

    update

--- .config/qtile/misc/toggle_redshift.sh
+++ .config/qtile/misc/toggle_redshift.sh
@@ -4,8 +4,8 @@ REDSHIFT_PID=$(pidof -s redshift)
 
 if [[ $? == 0 ]] ; then
     kill -s USR1 $REDSHIFT_PID
-    notify-send --app-name="redshift-toggle.sh" --expire-time=2500 "Toggled Redshift"
+    notify-send --app-name="toggle_redshift.sh" "Toggled Redshift"
 else
     echo "Could not find PID of redshift!" 1>&2
-    notify-send --app-name="redshift-toggle.sh" --expire-time=2500 --urgency=critical "Could not find PID of redshift!"
+    notify-send --app-name="redshift-toggle_redshift.sh" --urgency=critical "Could not find PID of redshift!"
 fi
